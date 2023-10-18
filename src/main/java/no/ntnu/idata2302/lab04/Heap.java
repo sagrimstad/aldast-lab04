7c4dee5b0f8b41627335bc010812347d16ddec3d
Sander Grimstad <sander.grimstad00@gmail.com>
2023-10-18 11:26:57 +0200

    Reformatted according to checkstyle

--- src/main/java/no/ntnu/idata2302/lab04/Heap.java
+++ src/main/java/no/ntnu/idata2302/lab04/Heap.java
@@ -14,63 +14,63 @@ public class Heap {
 
   private ArrayList<Integer> array;
 
-    public Heap() {
-        array = new ArrayList<Integer>();
-        // array.add(0);
-    }
+  public Heap() {
+    array = new ArrayList<Integer>();
+    // array.add(0);
+  }
 
-    /**
-     * Inserts a specified value to the binary heap.
-     * 
-     * <p>The heap is sorted after the insertion by going through each parent node from the inserted
-     * node and using bubble up operations if necessary.</p>
-     * 
-     * @param k A specified value to be insterted
-     */
-    public void insert(Integer k) {
-        array.add(k);
-        // Bubble up
-        int nodeIndex = array.size() - 1;
-        while (nodeIndex != 0) {
-            int parentIndex = parentOf(nodeIndex);
-            int difference = array.get(parentIndex).compareTo(array.get(nodeIndex));
-            if (difference > 0) {
-                swap(parentIndex, nodeIndex);
-            }
-            nodeIndex = parentIndex;
-        }
+  /**
+   * Inserts a specified value to the binary heap.
+   *
+   * <p>The heap is sorted after the insertion by going through each parent node from the inserted
+   * node and using bubble up operations if necessary.</p>
+   *
+   * @param k A specified value to be insterted
+   */
+  public void insert(Integer k) {
+    array.add(k);
+    // Bubble up
+    int nodeIndex = array.size() - 1;
+    while (nodeIndex != 0) {
+      int parentIndex = parentOf(nodeIndex);
+      int difference = array.get(parentIndex).compareTo(array.get(nodeIndex));
+      if (difference > 0) {
+        swap(parentIndex, nodeIndex);
+      }
+      nodeIndex = parentIndex;
     }
+  }
 
-    /**
-     * Returns the minimum value of the binary heap and deletes it.
-     * 
-     * <p>The heap is sorted after the deletion by going through each child node from the root node
-     * and using bubble down operations if necessary.</p>
-     * 
-     * @return The minimum value of the binary heap and deletes it
-     */
-    public int takeMinimum() {
-        int lastIndex = array.size() - 1;
-        swap(lastIndex, 0);
-        int minimum = array.remove(lastIndex);
-        // Bubble down
-        int parentIndex = 0;
-        while (array.indexOf(leftChildOf(parentIndex)) <= lastIndex) {
-            int minimumChildIndex;
-            if (array.indexOf(rightChildOf(parentIndex)) <= lastIndex) {
-                minimumChildIndex = Math.min(leftChildOf(parentIndex), rightChildOf(parentIndex));
-            } else {
-                minimumChildIndex = leftChildOf(parentIndex);
-            }
-            int difference = array.get(parentIndex).compareTo(array.get(minimumChildIndex));
-            if (difference <= 0) {
-                break;
-            }
-            swap(parentIndex, minimumChildIndex);
-            parentIndex = minimumChildIndex;
-        }
-        return minimum;
+  /**
+   * Returns the minimum value of the binary heap and deletes it.
+   *
+   * <p>The heap is sorted after the deletion by going through each child node from the root node
+   * and using bubble down operations if necessary.</p>
+   *
+   * @return The minimum value of the binary heap and deletes it
+   */
+  public int takeMinimum() {
+    int lastIndex = array.size() - 1;
+    swap(lastIndex, 0);
+    int minimum = array.remove(lastIndex);
+    // Bubble down
+    int parentIndex = 0;
+    while (array.indexOf(leftChildOf(parentIndex)) <= lastIndex) {
+      int minimumChildIndex;
+      if (array.indexOf(rightChildOf(parentIndex)) <= lastIndex) {
+        minimumChildIndex = Math.min(leftChildOf(parentIndex), rightChildOf(parentIndex));
+      } else {
+        minimumChildIndex = leftChildOf(parentIndex);
+      }
+      int difference = array.get(parentIndex).compareTo(array.get(minimumChildIndex));
+      if (difference <= 0) {
+        break;
+      }
+      swap(parentIndex, minimumChildIndex);
+      parentIndex = minimumChildIndex;
     }
+    return minimum;
+  }
 
   public void decreaseKey(int i, int k) {
     // TODO: Implement this operation
@@ -81,13 +81,13 @@ public class Heap {
     return index / 2;
   }
 
-    private int leftChildOf(int index) {
-        return index * 2 + 1;
-    }
+  private int leftChildOf(int index) {
+    return index * 2 + 1;
+  }
 
-    private int rightChildOf(int index) {
-        return index * 2 + 2;
-    }
+  private int rightChildOf(int index) {
+    return index * 2 + 2;
+  }
 
   void swap(int pos1, int pos2) {
     int temp = array.get(pos1);
